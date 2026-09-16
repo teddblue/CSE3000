48d660f0cb0af8dd0221228ea93533f6302d427a
teddblue <89668804+teddblue@users.noreply.github.com>
2026-09-16 15:22:36 -0400

    HW2 commit 3

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,10 +10,10 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.001,
-        n_estimators=1000,
-        max_depth=4,
-        subsample=0.5,
+        learning_rate=0.1,
+        n_estimators=100,
+        max_depth=2,
+        subsample=1.0,
         min_samples_leaf=1,
         random_state=seed
     )
